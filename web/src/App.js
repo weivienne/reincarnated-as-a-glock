5b5439bce8c352d2bd959826411a4072c602b816
Wei-Wern Ooi <viv@native.dev>
2025-01-17 16:19:40 -0500

    task: adds web app

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+        <iframe
+          width="100%"
+          height="720px"
+          border="none"
+          src="https://reincarnated-as-a-glock.vercel.app/"
+          title="Reincarnated as a Glock"
+        ></iframe>
+    </div>
+  );
+}
+
+export default App;
