d9685f3581bbfce142b79c17d580a81da73934ba
Wei-Wern Ooi <viv@native.dev>
2025-01-19 23:50:41 -0500

    task: adds act 1 storyboard

--- game/src/constants/index.js
+++ game/src/constants/index.js
@@ -2,6 +2,20 @@ import panel1 from "../panels/panel1.png";
 import panel2 from "../panels/panel2.png";
 import panel3 from "../panels/panel3.png";
 import panel4 from "../panels/panel4.png";
+import panel5 from "../panels/panel5.png";
+import panel6 from "../panels/panel6.png";
+import panel7 from "../panels/panel7.png";
+import panel8 from "../panels/panel8.png";
+import panel9 from "../panels/panel9.png";
+import panel10 from "../panels/panel10.png";
+import panel11 from "../panels/panel11.png";
+import panel12 from "../panels/panel12.png";
+import panel13 from "../panels/panel13.png";
+import panel14 from "../panels/panel14.png";
+import panel15 from "../panels/panel15.png";
+import panel16 from "../panels/panel16.png";
+import panel17 from "../panels/panel17.png";
+import panel18 from "../panels/panel18.png";
 
 export const PANELS = [
   {
@@ -16,7 +30,7 @@ export const PANELS = [
   {
     id: 1,
     background: panel2,
-    mc_dialogue: "This is another test.",
+    mc_dialogue: "Test.",
     mc_dialogue_x: "-2.7rem",
     mc_dialogue_y: "10rem",
     other_dialogues: () => [],
@@ -25,7 +39,7 @@ export const PANELS = [
   {
     id: 2,
     background: panel3,
-    mc_dialogue: "null",
+    mc_dialogue: " ",
     mc_dialogue_x: "2.7rem",
     mc_dialogue_y: "10rem",
     other_dialogues: () => [],
@@ -42,8 +56,8 @@ export const PANELS = [
   },
   {
     id: 4,
-    background: panel4,
-    mc_dialogue: "null",
+    background: panel5,
+    mc_dialogue: " ",
     mc_dialogue_x: "2.7rem",
     mc_dialogue_y: "10rem",
     other_dialogues: () => [],
@@ -51,11 +65,120 @@ export const PANELS = [
   },
   {
     id: 5,
-    background: panel4,
+    background: panel6,
     mc_dialogue: " ",
     mc_dialogue_x: "2.7rem",
     mc_dialogue_y: "10rem",
     other_dialogues: () => [],
     next_id: 6,
+  },
+
+  {
+    id: 6,
+    background: panel7,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [`This is a typing game.`],
+    next_id: 7,
+  },
+  {
+    id: 7,
+    background: panel8,
+    mc_dialogue: " ",
+    mc_dialogue_x: "-2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 8,
+  },
+  {
+    id: 8,
+    background: panel9,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 9,
+  },
+  {
+    id: 9,
+    background: panel10,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 10,
+  },
+  {
+    id: 10,
+    background: panel11,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 11,
+  },
+  {
+    id: 11,
+    background: panel12,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 12,
+  },
+  {
+    id: 12,
+    background: panel13,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [`This is a typing game.`],
+    next_id: 13,
+  },
+  {
+    id: 13,
+    background: panel14,
+    mc_dialogue: " ",
+    mc_dialogue_x: "-2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 14,
+  },
+  {
+    id: 14,
+    background: panel15,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 15,
+  },
+  {
+    id: 15,
+    background: panel16,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 16,
+  },
+  {
+    id: 16,
+    background: panel17,
+    mc_dialogue: "null",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 17,
+  },
+  {
+    id: 17,
+    background: panel18,
+    mc_dialogue: " ",
+    mc_dialogue_x: "2.7rem",
+    mc_dialogue_y: "10rem",
+    other_dialogues: () => [],
+    next_id: 18,
   },
 ];
